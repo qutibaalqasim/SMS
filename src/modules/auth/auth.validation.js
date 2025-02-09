11aab70c15d30b7f1a25a6cafc672c1e1b83d2d4
qutibaalqasim <qalqasim9@gmail.com>
2025-02-09 15:38:29 +0300

    create validation for register and login

---
+++ src/modules/auth/auth.validation.js
@@ -0,0 +1,15 @@
+import joi, { required } from "joi";
+
+
+export const registerSchema = joi.object({
+    userName: joi.string().min(3).max(20).required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(6).required(),
+});
+
+
+
+export const loginSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().min(6).required(),
+});
