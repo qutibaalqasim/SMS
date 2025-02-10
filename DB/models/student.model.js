816fc9e5da7b733125ec178ad546ea56e7d11edf
qutibaalqasim <qalqasim9@gmail.com>
2025-02-10 18:07:04 +0300

    create student model and the relation between two models

---
+++ DB/models/student.model.js
@@ -0,0 +1,30 @@
+import { BelongsTo, DataTypes, HasMany } from "sequelize";
+import { sequelize } from "../connection.js";
+import userModel from "./user.model.js";
+
+
+const studentModel = sequelize.define({
+    studentName:{
+        type: DataTypes.STRING,
+        allowNull:false,
+        unique:true
+    },
+    university:{
+        type: DataTypes.STRING,
+        allowNull:false
+    },
+    grade:{
+        type: DataTypes.INTEGER,
+        allowNull:false
+    },
+    studentPic:{
+        type: DataTypes.STRING,
+        allowNull:true
+    }
+
+});
+
+userModel.hasMany(studentModel);
+studentModel.belongsTo(userModel);
+
+export default studentModel;
