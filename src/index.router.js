f4c97726019384d5c5d247276c0b10a99e03633f
qutibaalqasim <qalqasim9@gmail.com>
2025-02-12 15:55:09 +0300

    connection DB router for welcome and Astric

--- src/index.router.js
+++ src/index.router.js
@@ -2,8 +2,14 @@ import { connectDB } from "../DB/connection.js";
 import authRouter from "./modules/auth/auth.router.js";
 import userRouter from "./modules/user/user.router.js";
 import studentRouter from "./modules/student/student.router.js";
+import cors from 'cors';
 const initApp = (app)=>{
     connectDB();
+    app.use(cors());
+    
+    app.get('/', (req, res)=>{
+        return res.status(200).json({message:"Welcome to the Student Management System"});
+    });
 
     // localhost:3000/auth/
     app.use('/auth', authRouter);
@@ -11,6 +17,10 @@ const initApp = (app)=>{
     app.use('/user', userRouter);
     // localhost:3000/student/
     app.use('/student', studentRouter);
+
+    app.get('*', (req,res)=>{
+        return res.status(404).json({message:"page not found"})
+    });
     //global error handler
     app.use( (err,req,res,next)=>{
         return res.status(err.statusCode).json({message:err.message});
