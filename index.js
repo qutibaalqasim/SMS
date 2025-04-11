d77c25b73d719b4874620b47f18e7d1f460b35b2
qutibaalqasim <qalqasim9@gmail.com>
2025-04-11 20:54:37 +0300

    modify the .gitignore file

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import initApp from './src/index.router.js';
 
 const app = express();
 app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
 initApp(app);
 
 
 
-app.listen(3000,()=>{
-    console.log('Server started on port 3000....');
+app.listen(PORT,()=>{
+    console.log(`Server started on port ${PORT}....`);
 })
