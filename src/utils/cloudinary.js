da1581b96a1d0fe4f4bad2f936a06ed4ee6fb9f3
qutibaalqasim <qalqasim9@gmail.com>
2025-04-11 20:51:06 +0300

    modify the cloudinary folder

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({ 
-    cloud_name: 'dtk8f8ago', 
-    api_key: '261599685319259', 
-    api_secret: 'PJXbAYSCZc4ThSUnBDggp45_YPI' // Click 'View API Keys' above to copy your API secret
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET // Click 'View API Keys' above to copy your API secret
 });
 
 export default cloudinary;
