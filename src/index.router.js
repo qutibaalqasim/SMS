478e39cb977cb2e577c0cfcfa83cf05fe7afa912
qutibaalqasim <qalqasim9@gmail.com>
2025-04-12 14:09:55 +0300

    delete all unused files

--- src/index.router.js
+++ src/index.router.js
@@ -1,7 +1,5 @@
 import { connectDB } from "../DB/connection.js";
 import authRouter from "./modules/auth/auth.router.js";
-import userRouter from "./modules/user/user.router.js";
-import studentRouter from "./modules/student/student.router.js";
 import cors from 'cors';
 const initApp = (app)=>{
     connectDB();
@@ -13,10 +11,7 @@ const initApp = (app)=>{
 
     // localhost:3000/auth/
     app.use('/auth', authRouter);
-    // localhost:3000/user/
-    app.use('/user', userRouter);
-    // localhost:3000/student/
-    app.use('/student', studentRouter);
+    
 
     app.get('*', (req,res)=>{
         return res.status(404).json({message:"page not found"})
--- src/modules/user/user.validation.js
+++
@@ -1,8 +0,0 @@
-import joi from "joi";
-
-
-export const idValidate = joi.object({
-    id: joi.number().min(1).required()
-});
-
-
