c64a0180bb82283741e9f2857b21fa4fe855d41c
qutibaalqasim <qalqasim9@gmail.com>
2025-06-11 15:12:07 +0300

    modify createREaction and deleteReaction end points

--- src/modules/reaction/reaction.controller.js
+++ src/modules/reaction/reaction.controller.js
@@ -1,3 +1,4 @@
+import postModel from "../../../DB/models/post.model.js";
 import reactionModel from "../../../DB/models/reaction.model.js";
 import { AppError } from "../../utils/AppError.js";
 
@@ -8,6 +9,18 @@ export const createReaction = async (req, res, next) => {
     if (!postId || !emoji) {
         return next(new AppError("Post ID and emoji are required", 400));
     }
+
+    const existing = await reactionModel.findOne({
+        where: {
+            postId,
+            emoji,
+            userId: req.id
+        }
+    });
+
+    if (existing) {
+        return next(new AppError("You have already reacted with this emoji", 400));
+    }
     const reaction = await reactionModel.create({
         emoji,
         userId: req.id,
@@ -17,6 +30,10 @@ export const createReaction = async (req, res, next) => {
     if(!reaction) {
         return next(new AppError("Failed to create reaction", 400));
     }
+
+    await postModel.increment('likesCount', {
+        where: { id: postId }
+    });
 
     return res.status(201).json({ message: "Reaction created successfully", reaction });
 }
@@ -40,6 +57,10 @@ export const deleteReaction = async (req, res, next) => {
     if (!reaction) {
         return next(new AppError("Failed to delete reaction", 400));
     }
+
+    await postModel.decrement('likesCount', {
+        where: { id: postId }
+    });
 
     return res.status(200).json({ message: "Reaction deleted successfully" });
 }
