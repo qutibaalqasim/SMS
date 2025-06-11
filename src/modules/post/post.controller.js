e5da91d7c26016f16347322b5e5ab954976722b0
qutibaalqasim <qalqasim9@gmail.com>
2025-06-11 12:08:58 +0300

    create getUserPosts and getPostDetails end points

--- src/modules/post/post.controller.js
+++ src/modules/post/post.controller.js
@@ -46,3 +46,36 @@ export const getPosts = async (req, res, next) => {
 
     return res.status(200).json({message:"success", posts});
 }
+
+export const getUserPosts = async (req, res, next) => {
+    const posts = await postModel.findAll({
+        where: { userId: req.id },
+        include: [{
+            model: postImageModel,
+            as: 'images'
+        }],
+    });
+
+    if (!posts || posts.length == 0) {
+        return next(new AppError("No posts found for this user", 404));
+    }
+
+    return res.status(200).json({message:"success", posts});
+}
+
+export const getPostDetails = async (req, res, next) => {
+    const { postId } = req.params;
+    const post = await postModel.findOne({
+        where: { id: postId },
+        include: [{
+            model: postImageModel,
+            as: 'images'
+        }],
+    });
+
+    if (!post) {
+        return next(new AppError("Post not found", 404));
+    }
+
+    return res.status(200).json({message:"success", post});
+}
--- src/modules/post/post.router.js
+++ src/modules/post/post.router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { auth } from "../../midleware/auth.js";
 import { asyncHandler } from "../../utils/catchError.js";
-import { createPost, getPosts } from "./post.controller.js";
+import { createPost, getPostDetails, getPosts, getUserPosts } from "./post.controller.js";
 import fileUpload from "../../utils/multer.js";
 
 
@@ -9,7 +9,8 @@ import fileUpload from "../../utils/multer.js";
 const router = Router();
 
 router.post('/', auth(['instructor','admin','university_admin']),fileUpload().array('images', 5),asyncHandler(createPost));
-
 router.get('/', auth(['admin']), asyncHandler(getPosts));
+router.get('/user', auth(['instructor','admin','university_admin']), asyncHandler(getUserPosts));
+router.get('/:postId', auth(['admin']), asyncHandler(getPostDetails));
 
 export default router;
